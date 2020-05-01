494e0ac62f91005482fbb6be123f31a7ebad373c
Nicholas Soper <nick@rocketmakers.com>
2020-05-01 16:29:41 +0100

    Logging

--- socket.js
+++ socket.js
@@ -5,6 +5,7 @@ const socket = io('https://niksoper-future-joinery-api.eu.ngrok.io')
 socket.on('connection', () => console.log('Got socket.io connection!'))
 
 function send(data) {
+  console.log('Sending socket message', data)
   socket.emit('onshape', data)
 }
 
