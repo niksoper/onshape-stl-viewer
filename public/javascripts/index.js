e5c930691af6d331e3642aa33b19bf85e885b5d5
Nicholas Soper <nick@rocketmakers.com>
2020-05-01 15:45:20 +0100

    USe dataType text for STL

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -14,7 +14,7 @@ function displayOutput(output) {
 }
 
 function getElements() {
-  return apiGet('Getting elements', '/api/elements', function(elements) {
+  return apiGet('Getting elements', 'json', '/api/elements', function(elements) {
     var partStudio = elements.find(e => e.elementType === 'PARTSTUDIO')
     if (partStudio) {
       partStudioElementId = partStudio.id;
@@ -28,25 +28,26 @@ function getElements() {
 }
 
 function getParts() {
-  return apiGet('Getting parts', '/api/parts', function(parts) {
+  return apiGet('Getting parts', 'json', '/api/parts', function(parts) {
     displayOutput(JSON.stringify(parts, null, 2))
   })
 }
 
 function getConfiguration() {
   var queryParams = '?documentId=' + getQueryParam('documentId') + '&workspaceId=' + getQueryParam('workspaceId') + '&elementId=' + partStudioElementId
-  return apiGet('Getting configuration', '/api/configuration', function(parts) {
+  return apiGet('Getting configuration', 'json', '/api/configuration', function(parts) {
     displayOutput(JSON.stringify(parts, null, 2))
   }, queryParams)
 }
 
 function getTest() {
-  return apiGet('Running test', '/api/test', function(result) {
+  return apiGet('Running test', 'text', '/api/test', function(result) {
+    console.log('RESULT', result)
     displayOutput(result)
   })
 }
 
-function apiGet(placeholder, apiPath, onResponse, queryParams) {
+function apiGet(placeholder, dataType, apiPath, onResponse, queryParams) {
   displayOutput(placeholder)
 
   if (!queryParams) {
@@ -55,7 +56,7 @@ function apiGet(placeholder, apiPath, onResponse, queryParams) {
 
   var dfd = $.Deferred();
   $.ajax(apiPath + queryParams, {
-      dataType: 'json',
+      dataType: dataType,
       type: 'GET',
       success: onResponse,
       error: displayOutput,
